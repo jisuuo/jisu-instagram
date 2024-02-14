b1574d8e3ebf277c51b997c1cd6e89d38bf4235b
JISUOGIT <jisu2298@gmail.com>
2024-02-15 00:07:57 +0900

    statusCode interceptor 수정 1. 200 fix -> statusCode 변수로 변경

--- src/common/inceptors/transform.interceptor.ts
+++ src/common/inceptors/transform.interceptor.ts
@@ -7,15 +7,26 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export const Info = {
-  statusCode: 200,
-  message: 'success',
+// export const Info = {
+//   statusCode: 200,
+//   message: 'success',
+// };
+
+export function createInfo(statusCode: number = 200) {
+  return {
+    statusCode,
+    message: 'success',
+  };
+}
+
+export type Response<T> = ReturnType<typeof createInfo> & {
+  data: T;
 };
 
 // 성공시 던져주는 msg 형식
-export type Response<T> = typeof Info & {
-  data: T;
-};
+// export type Response<T> = typeof Info & {
+//   data: T;
+// };
 
 @Injectable()
 export class TransformInterceptor<T>
@@ -25,6 +36,10 @@ export class TransformInterceptor<T>
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<Response<T>> {
-    return next.handle().pipe(map((data) => Object.assign({}, Info, { data })));
+    const res = context.switchToHttp().getResponse();
+    const status = res.statusCode;
+    return next
+      .handle()
+      .pipe(map((data) => Object.assign({}, createInfo(status), { data })));
   }
 }
